b82db15f23acb3febb39da10230b5c03dbbc019f
Dong-Yeop <ehdduq53@gmail.com>
2022-08-18 00:46:52 +0900

    라우터 셋팅

---
+++ vue-news/src/components/ToolBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="header">
+    <router-link to="/News">News</router-link> |
+    <router-link to="/Ask">Ask</router-link> |
+    <router-link to="/Jobs">Jobs</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.header {
+  color:#fff;
+  background:#42b883;
+  display:flex;
+  padding:8px;
+}
+.header a {
+  color:#fff;
+}
+.header a.router-link-exact-active {
+  color:#35495e;
+}
+</style>
